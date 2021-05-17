d14ff4a3d81ff1586382b182c6ab1cfa116093f2
Vojtech Horky <vojtech.horky@gmail.com>
2021-05-17 20:43:40 +0200

    GHA: dump versions

--- tools/ci/show-env.sh
+++ tools/ci/show-env.sh
@@ -10,3 +10,10 @@ git --version || echo "Git not installed." >&2
 
 cat /etc/os-release
 uname -a
+
+source "$(dirname "$0")/common.sh"
+echo
+echo "RENAISSANCE_GIT_VERSION='$RENAISSANCE_GIT_VERSION'"
+echo "RENAISSANCE_JAR_NAME='$RENAISSANCE_JAR_NAME'"
+echo "RENAISSANCE_JMH_JAR_NAME='$RENAISSANCE_JMH_JAR_NAME'"
+echo

2014490919095fd264867dbc579b1f0bdf0ca405
Vojtech Horky <vojtech.horky@gmail.com>
2021-06-08 11:24:21 +0200

    GHA: clean-up

--- tools/ci/check-jmh.sh
+++ tools/ci/check-jmh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 source "$(dirname "$0")/common.sh"
 
-ls -l "$RENAISSANCE_JMH_JAR" || find "$( dirname "$( dirname "$RENAISSANCE_JMH_JAR" )" )" -name '*.jar'
-
 # Ensure that the JMH bundle contains non-empty BenchmarkList
 test $(unzip -p "$RENAISSANCE_JMH_JAR" META-INF/BenchmarkList | wc -l) -gt 0
